cd19c08bc8e0c77b572e6b78b4f74ebe8184bfa6
Mortreddd <emmanmale@gmail.com>
2024-11-26 21:23:17 +0800

    Fixed notifying user

--- src/components/modals/NotifyMessageModal.tsx
+++ src/components/modals/NotifyMessageModal.tsx
@@ -1,20 +1,62 @@
 import { Transaction } from "@/types/Transactions";
-import { forwardRef, Ref } from "react";
+import { FormEvent, forwardRef, Ref, useState } from "react";
 import Modal, { ModalRef } from "../common/Modal";
 import { Button } from "../common/Button";
-
+import { useAlert } from "@/contexts/AlertContext";
+import { ADMIN_API } from "@/utils/Api";
+import { AxiosResponse, AxiosError } from "axios";
+import { ErrorResponse, Response } from "@/types/Models";
 interface NotifyMessageModalProps {
   transaction: Transaction | null;
-  onConfirm: () => void;
+  onNotifying: (isLoading: boolean) => void;
 }
 
 function NotifyMessageModal(
-  { transaction, onConfirm }: NotifyMessageModalProps,
+  { transaction, onNotifying }: NotifyMessageModalProps,
   ref: Ref<ModalRef>
 ) {
+  const [message, setMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { showAlert } = useAlert();
+
+  function onChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const value = e.target.value;
+    setMessage(value);
+  }
+
+  async function notifyUser(e: FormEvent) {
+    e.preventDefault();
+    onNotifying(true);
+    setIsLoading(true);
+    await ADMIN_API.post(`/dashboard/transactions/${transaction?.id}/notify`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response: AxiosResponse<Response>) => {
+        onNotifying(false);
+        setIsLoading(false);
+        showAlert(
+          response.data.message ?? "User notified successfully",
+          "success"
+        );
+      })
+      .catch((error: AxiosError<ErrorResponse>) => {
+        onNotifying(false);
+        setIsLoading(false);
+        showAlert(
+          error.response?.data.message ?? "Something went wrong",
+          "error"
+        );
+      });
+  }
+
   return (
     <Modal ref={ref}>
-      <div className="w-full font-sans h-fit bg-white flex flex-col items-center gap-5">
+      <form
+        onSubmit={notifyUser}
+        className="w-full font-sans h-fit bg-white flex flex-col items-center gap-5"
+      >
         <div className="w-full flex justify-center">
           <h1 className="md:text-2xl text-xl font-sans text-gray-700">
             Notify {transaction?.user?.fullName}
@@ -22,21 +64,25 @@ function NotifyMessageModal(
         </div>
         <div className="w-full my-5">
           <textarea
+            onChange={onChange}
             className="w-full h-32 p-2 border border-gray-300 rounded-md"
             placeholder="Enter your message here..."
-          ></textarea>
+          >
+            {message}
+          </textarea>
         </div>
         <div className="w-full">
           <Button
-            onClick={onConfirm}
+            type={"submit"}
             variant={"success"}
             rounded={"default"}
+            loading={isLoading}
             className="w-full"
           >
             Send
           </Button>
         </div>
-      </div>
+      </form>
     </Modal>
   );
 }
